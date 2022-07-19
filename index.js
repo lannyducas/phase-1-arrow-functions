d662fc687a9748f587b02382066029d3882aa200
Danny <lannyducas@gmail.com>
2022-07-19 13:25:50 -0400

    commit tree 2700a9f0cfabf393a14485c7ccfbfdb488c37c99

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = () => {
+    return (2000 / 100);
+}
+const square = (parameter1) => {
+    return parameter1 * parameter1;
+}
+
+const add = (parameter1, parameter2) => {
+    return parameter1 + parameter2;
+}
